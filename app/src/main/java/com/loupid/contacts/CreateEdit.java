506d3c393fef8220e72e0e2906a2fe3cb848490d
Loupid <loupidmasta@gmail.com>
2020-09-23 23:08:59 -0400

    nothing change

--- app/src/main/java/com/loupid/contacts/CreateEdit.java
+++ app/src/main/java/com/loupid/contacts/CreateEdit.java
@@ -68,6 +68,13 @@ public class CreateEdit extends AppCompatActivity {
             }
         }
 
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         final int finalPosition = position;
 
         confirm.setOnClickListener(new View.OnClickListener() {
@@ -93,11 +100,13 @@ public class CreateEdit extends AppCompatActivity {
 
             private boolean isValidate() {
                 boolean approve = true;
+
                 Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+\\=?^_`{|}~-]+" +
                         "(?:\\.[a-z0-9!#$%&'*+\\=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")" +
                         "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" +
                         "|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:" +
                         "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+
                 Pattern phonePattern = Pattern.compile("^[0-9]{0,1}\\([0-9]{3}\\)\\ {1}[0-9]{3}\\-[0-9]{4}$");
                 if (firstname.getText().toString().trim().isEmpty()){
                     firstname.setError("Le champ prénom ne peut pas être vide");
